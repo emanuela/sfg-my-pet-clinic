7e5c51858483ffd69b4a0f40de4f0a85435012f9
Emanuel Alongi <emanuel.alongi@cognizant.com>
2020-05-05 15:30:35 -0600

    Initial project add.

---
+++ src/main/java/com/emanuel/sfgmypetclinic/SfgMyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.emanuel.sfgmypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMyPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgMyPetClinicApplication.class, args);
+	}
+
+}
