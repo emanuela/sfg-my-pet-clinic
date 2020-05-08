c5c39cb15a6b287e37dec715d4ca2332caadabb3
Emanuel Alongi <emanuel.alongi@cognizant.com>
2020-05-08 09:45:10 -0600

    Adding POJOs.

---
+++ src/main/java/com/emanuel/sfgmypetclinic/model/Person.java
@@ -0,0 +1,38 @@
+/*
+ * COGNIZANT CONFIDENTIAL AND/OR TRADE SECRET
+ *
+ * Copyright [2015] - [2020] Cognizant.  All rights reserved.
+ *
+ * NOTICE: This unpublished material is proprietary to Cognizant and
+ * its suppliers, if any.  The methods, techniques and technical
+ * concepts herein are considered Cognizant confidential ane/or trade
+ * secret information.  This material may be covered by U.S. and/or
+ * foreign patents or patent applications.  Use, distribution or
+ * copying, in whole or in part, is forbidden, except by express written
+ * permission of Cognizant.
+ *
+ */
+
+package com.emanuel.sfgmypetclinic.model;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
