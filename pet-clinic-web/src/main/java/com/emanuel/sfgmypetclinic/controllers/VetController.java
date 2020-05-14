6c2bc6b14c82b939024215208adcba46ddcd4702
Emanuel Alongi <emanuel.alongi@cognizant.com>
2020-05-14 15:52:37 -0600

    Adding vets page and controller. Closes #6.

---
+++ pet-clinic-web/src/main/java/com/emanuel/sfgmypetclinic/controllers/VetController.java
@@ -0,0 +1,29 @@
+/*
+ * COGNIZANT CONFIDENTIAL AND/OR TRADE SECRET
+ *
+ * Copyright [2015] - [2020] Cognizant.  All rights reserved.
+ *
+ * NOTICE: This unpublished material is proprietary to Cognizant and
+ * its suppliers, if any.  The methods, techniques and technical
+ * concepts herein are considered Cognizant confidential ane/or trade
+ * secret information.  This material may be covered by U.S. and/or
+ * foreign patents or patent applications.  Use, distribution or
+ * copying, in whole or in part, is forbidden, except by express written
+ * permission of Cognizant.
+ *
+ */
+
+package com.emanuel.sfgmypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        return "vets/index";
+    }
+}
