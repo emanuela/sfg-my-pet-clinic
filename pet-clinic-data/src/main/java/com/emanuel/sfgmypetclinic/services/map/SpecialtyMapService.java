cf942b2fd9afdd126a01caafc7c985da07dc554e
Emanuel Alongi <emanuel.alongi@cognizant.com>
2020-06-03 15:37:04 -0600

    Added Specialties Map Impl. Closes #25.

---
+++ pet-clinic-data/src/main/java/com/emanuel/sfgmypetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,51 @@
+/*
+ * COGNIZANT CONFIDENTIAL AND/OR TRADE SECRET
+ *
+ * Copyright [2015] - [2020] Cognizant.  All rights reserved.
+ *
+ * NOTICE: This unpublished material is proprietary to Cognizant and
+ * its suppliers, if any.  The methods, techniques and technical
+ * concepts herein are considered Cognizant confidential ane/or trade
+ * secret information.  This material may be covered by U.S. and/or
+ * foreign patents or patent applications.  Use, distribution or
+ * copying, in whole or in part, is forbidden, except by express written
+ * permission of Cognizant.
+ *
+ */
+
+package com.emanuel.sfgmypetclinic.services.map;
+
+import com.emanuel.sfgmypetclinic.model.Specialty;
+import com.emanuel.sfgmypetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
