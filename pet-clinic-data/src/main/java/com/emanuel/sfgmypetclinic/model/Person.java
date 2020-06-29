a6dd29b86d2943674b236d885fae4615fd7916e0
emanuelalongi <emanuel.alongi@gmail.com>
2020-06-29 15:40:42 -0600

    Updated Owner with JPA mappings. Closes #30.

--- pet-clinic-data/src/main/java/com/emanuel/sfgmypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/emanuel/sfgmypetclinic/model/Person.java
@@ -15,9 +15,16 @@
 
 package com.emanuel.sfgmypetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name="first_name")
     private String firstName;
+
+    @Column(name="last_name")
     private String lastName;
 
     public String getFirstName() {
